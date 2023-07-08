cbd629f33e61a3c7d6798d79350d8ec103a22ca9
DESKTOP-55KN8RI\Marlene <marlenebr@outlook.de>
2023-07-08 22:22:54 +0200

    player am start

--- gamejam2023/Assets/Scripts/GameManager.cs
+++ gamejam2023/Assets/Scripts/GameManager.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private Map ShopMap;
+
+    public Player ShopPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ShopMap = this.GetComponent<Map>();
+        if(ShopMap.initialized == true) 
+        {
+            SetPlayer();
+        }
+
+        else
+        {
+            ShopMap.isInitializationDone.AddListener(SetPlayer);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SetPlayer()
+    {
+        ShopPlayer.transform.position = ShopMap.GetEntrance();
+        Debug.LogError(ShopPlayer.transform.position);
     }
 }

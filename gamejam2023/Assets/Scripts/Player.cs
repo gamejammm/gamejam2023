52fb7c8887f83f35f29a4a650b936819627ba260
DESKTOP-55KN8RI\Marlene <marlenebr@outlook.de>
2023-07-25 21:11:37 +0200

    mit Steuerknüppel an controller - linker stick

--- gamejam2023/Assets/Scripts/Player.cs
+++ gamejam2023/Assets/Scripts/Player.cs
@@ -1,13 +1,15 @@
 using DefaultNamespace;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public Vector3 right;
-    public Vector3 up;
-    public Vector3 camVector;
+    public float Speed = 5f;
+
+    public float PlayerHeight = 0.37f;
 
     public GameObject visualFront;
     public GameObject visualBack;
@@ -21,8 +23,28 @@ public class Player : MonoBehaviour
     public int LitterCount;
 
     private GameManager _gameManager;
-    
-    
+
+
+
+    bool modifierPressed;
+
+    Vector2 motion;
+
+
+    public void OnPlayerMove(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            motion = context.ReadValue<Vector2>();
+
+        }
+        else
+        {
+            motion  = Vector2.zero;
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,48 +59,98 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
+        {
+            SetMovePlayer(Vector2.zero);
+        }
 
-        Vector3 motion = new Vector3();
+
         if (Input.GetKey(KeyCode.W))
         {
-            motion += this.up * Time.deltaTime;
+            SetMovePlayer(Vector2.up);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            SetMovePlayer(Vector2.left);
+
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            SetMovePlayer(Vector2.down);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            SetMovePlayer(Vector2.right);
+        }
+
+        else
+        {
+            Gamepad gamepad = Gamepad.current;
+            // The return value of `.current` can be null.
+            if (gamepad != null && gamepad.leftStick.value != Vector2.zero)
+            {
+                motion = gamepad.leftStick.value;
+            }
+
+            SetMovePlayer(motion);
+
+        }
+
+        this.gameObject.transform.position = new Vector3(this.transform.position.x, PlayerHeight, this.transform.position.z);
+
+    }
+
+    private void SetMovePlayer(Vector2 direction)
+    {
+        if (direction == Vector2.zero)
+        {
+            motion = Vector2.zero;
+            return;
+        }
+
+        float degreeToMove = Vector2.Angle(Vector2.up, direction);
+
+        Debug.LogError(Vector2.Angle(Vector2.up,direction));
+
+        if (direction == Vector2.up)
+        {
+            motion = Vector2.up;
             visualBack.SetActive(true);
             visualFront.SetActive(false);
         }
-        if (Input.GetKey(KeyCode.A))
+        else if (direction == Vector2.left)
         {
-            motion -= this.right * Time.deltaTime;
+            motion = Vector2.left;
             visualBack.GetComponent<SpriteRenderer>().flipX = false;
             visualFront.GetComponent<SpriteRenderer>().flipX = true;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            motion -= this.up * Time.deltaTime;
             visualBack.SetActive(false);
             visualFront.SetActive(true);
         }
-        if (Input.GetKey(KeyCode.D))
+        else if (direction == Vector2.right)
         {
-            motion += this.right * Time.deltaTime;
+            motion = Vector2.right;
             visualBack.GetComponent<SpriteRenderer>().flipX = true;
             visualFront.GetComponent<SpriteRenderer>().flipX = false;
+            visualBack.SetActive(false);
+            visualFront.SetActive(true);
         }
-        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S)) {
+        else if (direction == Vector2.down)
+        {
+            motion = Vector2.down;
             visualBack.SetActive(false);
             visualFront.SetActive(true);
         }
 
-        if (_controller != null)
-        {
-            _controller.Move(motion);
-        }
+        _controller.Move(-new Vector3(motion.x, 0f, motion.y) * Time.deltaTime * Speed);
 
-        _SetPlayer();
     }
 
     protected void _SetPlayer()
     {
-        this.gameObject.transform.position = new Vector3(this.transform.position.x, 0.37f, this.transform.position.z);
+        visualBack.SetActive(false);
+        visualFront.SetActive(true);
+        this.gameObject.transform.position = new Vector3(this.transform.position.x, PlayerHeight, this.transform.position.z);
+
     }
 
     private void OnTriggerEnter(Collider other)
